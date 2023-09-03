8c4d1674953cd981aee26270402ed6dcb101c591
diego <tekadiego23@gmail.com>
2023-09-03 10:33:20 +0200

    adding git actions

--- customer-api/src/main/java/com/diego/code/learning/Main.java
+++ customer-api/src/main/java/com/diego/code/learning/Main.java
@@ -2,14 +2,12 @@ package com.diego.code.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class LearningApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearningApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 
